fcf01a0aed73608fa5e0fce2da9154a0362ed270
Martin Forsberg <marfo68@liu.se>
2023-08-30 17:51:51 +0200

    Adding profile prictures from admin page. Signals to add a default profile pricture on user creation

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+# Where uploaded files will be saved, creates a media folder in base
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Public URL when using in browser
+MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'Bootstrap4'
