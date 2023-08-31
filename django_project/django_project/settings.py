c537d5c7fefafa71c0413177b2930a403f88ff44
Martin Forsberg <marfo68@liu.se>
2023-08-31 17:30:15 +0200

    Password reset functionallity with gmail server

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -133,6 +133,13 @@ CRISPY_TEMPLATE_PACK = 'Bootstrap4'
 LOGIN_REDIRECT_URL ='blog-home'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('mail_server_user')
+EMAIL_HOST_PASSWORD = os.environ.get('mail_server_PW')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
