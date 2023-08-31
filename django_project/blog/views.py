1d367a0bcc4291c9baadee287c8fedb012a80549
Martin Forsberg <marfo68@liu.se>
2023-08-31 10:07:49 +0200

    Delete and update functionallity on the frontend

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (ListView,
                                   DetailView,
-                                  CreateView)
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView)
 from .models import Post
 
 # Create your views here.
@@ -31,6 +33,32 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
     
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        #Set the author to the current user
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    #MAke sure only the author of the post can edit a post
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
